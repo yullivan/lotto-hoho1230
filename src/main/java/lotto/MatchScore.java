3b3a91416478a94ca4586b0d99fcc9889736fa5a
TJOEUNHY <lee@>
2024-12-11 17:47:43 +0900

    당첨결과 변수 클래스 생성

---
+++ src/main/java/lotto/MatchScore.java
@@ -0,0 +1,11 @@
+package lotto;
+
+public class MatchScore {
+    private final String FIRST="6개 일치(2000000000원)";
+    private final String SECOND="5개 일치(1500000원)";
+    private final String THIRD="4개 일치(50000원)";
+    private final String FOURTH="3개 일치(5000원)";
+    private final String MISS="불일치";
+
+
+}
